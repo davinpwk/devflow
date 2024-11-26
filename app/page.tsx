f7c7a534938bd0af4a48161527cd308af7b9a53a
davinpwk <davinpwk@gmail.com>
2024-11-26 11:33:14 +0700

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@
 const Home = () => {
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of Next.js</h1>;
-      <h2 className="h2-bold">Welcome to the world of Next.js</h2>;
-      <h2 className="h2-semibold">Welcome to the world of Next.js</h2>;
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-inter">
+        Welcome to the world of Next.js (Inter)
+      </h1>
     </>
   );
 };
