40c8ca580b6daa513b4b68ae0eb891d922388dd2
davinpwk <davinpwk@gmail.com>
2024-12-18 10:04:12 +0700

    Mongoose Types

--- database/account.model.ts
+++ database/account.model.ts
@@ -1,14 +1,15 @@
-import { model, models, Schema, Types } from "mongoose";
+import { model, models, Schema, Types, Document } from "mongoose";
 
 export interface IAccount {
   userId: Types.ObjectId;
   name: string;
-  image: string;
-  password: string;
+  image?: string;
+  password?: string;
   provider: string;
   providerAccountId: string;
 }
 
+export interface IAccountDoc extends IAccount, Document {}
 const AccountSchema = new Schema<IAccount>(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
